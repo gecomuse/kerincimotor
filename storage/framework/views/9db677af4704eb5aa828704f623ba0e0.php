b91c0c41dd24b0ae6d3b2ee15c59a812c44dd480
gecomuse <dandymulyadi1@gmail.com>
2026-04-03 18:12:46 +0700

    fix gitignore - remove cache and tmp files

--- storage/framework/views/9db677af4704eb5aa828704f623ba0e0.php
+++
@@ -1,3 +0,0 @@
-<img src="<?php echo e(asset('images/logo.png')); ?>" alt="Kerinci Motor" class="h-8"
-     onerror="this.outerHTML='<span class=\'font-heading font-bold text-white text-lg\'>KERINCI<span class=\'text-red-500\'>MOTOR</span></span>'">
-<?php /**PATH D:\= vscode\Kerinci Motor\resources\views/filament/brand.blade.php ENDPATH**/ ?>
